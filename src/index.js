d188cc64440864a235384419a6f2d470b602f951
Luiza Sntg <lu.sntgo@gmail.com>
2022-01-29 18:39:22 -0300

    feat: return list of participants

--- src/index.js
+++ src/index.js
@@ -9,58 +9,57 @@ const server = express();
 server.use(cors());
 server.use(json());
 
-const participants = [];
+const users = [];
 
 server.get("/participants", (req, res) => {
   const mongoClient = new MongoClient(
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=chatUol"
+    "mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=chatUol"
   );
-  const promise = mongoClient.connect();
+  const promiseConnect = mongoClient.connect();
 
-  promise.then((connectedMongoClient) => {
+  promiseConnect.then((connectedMongoClient) => {
     console.log("SUCESSOOOO");
 
     const dbParticipants = connectedMongoClient.db("participants");
-    const participantsCollection = dbParticipants.collection("participants");
+    const participantsCollection = dbParticipants.collection("users");
 
     const promiseParticipants = participantsCollection.find({}).toArray();
 
-    promiseParticipants.then((participants) => {
-      res.send(participants);
+    promiseParticipants.then((users) => {
+      res.send(users);
       mongoClient.close();
     });
 
-    promise.catch((error) => {
+    promiseParticipants.catch((error) => {
       console.log("Error nos dados", error);
-      res.send(error);
+      res.sendStatus(500);
       mongoClient.close();
     });
   });
 
-  promise.catch((error) => {
-    console.log("Error", error);
-    res.send(error);
+  promiseConnect.catch((error) => {
+    console.log("Error de conexão", error);
+    res.send(500);
     mongoClient.close();
   });
 });
 
 server.post("/participants", (req, res) => {
-  const name = req.body;
-  if (name === "") {
-    res.status(422).send("Não deixe vazio");
+  if (!req.body.name || !req.body.lastStatus) {
+    res.status(422).send("Todos os campos são obrigatórios!");
     return;
   }
 
   const mongoClient = new MongoClient(
-    "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=chatUol"
+    "mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=chatUol"
   );
-  const promise = mongoClient.connect();
+  const promiseConnect = mongoClient.connect();
 
-  promise.then((connectedMongoClient) => {
+  promiseConnect.then((connectedMongoClient) => {
     console.log("SUCESSOOOO");
 
     const dbParticipants = connectedMongoClient.db("participants");
-    const participantsCollection = dbParticipants.collection("participants");
+    const participantsCollection = dbParticipants.collection("users");
 
     const promiseInsert = participantsCollection.insertOne(req.body);
 
@@ -70,15 +69,15 @@ server.post("/participants", (req, res) => {
     });
 
     promiseInsert.catch((error) => {
-      console.log("Error nos dados", error);
-      res.send(error);
+      console.log("Error no push de dados", error);
+      res.sendStatus(500);
       mongoClient.close();
     });
   });
 
-  promise.catch((error) => {
-    console.log("Error", error);
-    res.send(error);
+  promiseConnect.catch((error) => {
+    console.log("Error de conexão", error);
+    res.send(500);
     mongoClient.close();
   });
 
